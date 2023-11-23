3ddb05a593c13b2c64e119d2f2543b047158f25d
ALaa Ahmed <eng.alaaelgammall@gmail.com>
2023-11-23 13:04:43 +0200

    Edited script

--- AdminLinux_Task1/script.sh
+++ AdminLinux_Task1/script.sh
@@ -1,1 +1,8 @@
-# start of code
+#!bin/bash
+cd $Home
+mkdir "myDirectory"
+cd "myDirectory"
+mkdir "secondDirectory"
+touch "myNotePaper"
+cp "myNotePaper" "myDirectory"
+mv "myNotePaper" "myOldNotePaper
